0ae4a9aad9a3f0983c6d13220c6f00357fbe270c
Nikita110602 <nikitasharma110602@gmail.com>
2023-05-06 10:42:55 +0530

    first commit

---
+++ form-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './index.css'
+import MultipleInputs from './components/multipleinputs'
+
+const App = () => {
+  return (
+    <div><MultipleInputs/></div>
+  )
+}
+
+export default App
